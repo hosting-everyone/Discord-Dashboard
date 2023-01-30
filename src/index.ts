ca79bca1049d832c7a40c584f9cfc21b868f662b
Marcin Kondrat <breftejk@gmail.com>
2023-01-30 17:30:18 +0100

    WebSocket submodule

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { Dashboard } from '@/dashboard/dashboard';
 export { IThemeProvider } from '@/dashboard/dashboard.interface';
+export { IModuleProvider } from '@/modules/module.interface';
 export { Db } from '@/db/db';
